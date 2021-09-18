49b1b187659738af7cf3d0ecb399dd187689bef7
aam198 <aam198@psu.edu>
2021-09-18 09:26:35 -0400

    Linting

--- scripts/archive.js
+++ scripts/archive.js
@@ -66,7 +66,7 @@ function updateProgress(fileNumber, percent) {
 function handleFiles(files) {
   files = [...files]
   initializeProgress(files.length);
-  files.forEach(uploadFile);
+  // files.forEach(uploadFile); 
   files.forEach(listFile);
   if(files.length <= 0){
     nextBtn.setAttribute("disabled", "");
@@ -76,31 +76,31 @@ function handleFiles(files) {
   }
 }
 
-function uploadFile(file, i) {
-  const url = 'https://api.cloudinary.com/v1_1/joezimim007/image/upload';
-  const xhr = new XMLHttpRequest();
-  const formData = new FormData();
-  xhr.open('POST', url, true);
-  xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+// function uploadFile(file, i) {
+//   const url = 'https://api.cloudinary.com/v1_1/joezimim007/image/upload';
+//   const xhr = new XMLHttpRequest();
+//   const formData = new FormData();
+//   xhr.open('POST', url, true);
+//   xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
 
-  // Update progress (can be used to show progress indicator)
-  xhr.upload.addEventListener("progress", function(e) {
-    updateProgress(i, (e.loaded * 100.0 / e.total) || 100);
-  });
+//   // Update progress (can be used to show progress indicator)
+//   xhr.upload.addEventListener("progress", function(e) {
+//     updateProgress(i, (e.loaded * 100.0 / e.total) || 100);
+//   });
 
-  xhr.addEventListener('readystatechange', function(e) {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-      updateProgress(i, 100) // <- Add this
-    }
-    else if (xhr.readyState == 4 && xhr.status != 200) {
-      // Error. Inform the user
-    }
-  });
+//   xhr.addEventListener('readystatechange', function(e) {
+//     if (xhr.readyState == 4 && xhr.status == 200) {
+//       updateProgress(i, 100) // <- Add this
+//     }
+//     else if (xhr.readyState == 4 && xhr.status != 200) {
+//       // Error. Inform the user
+//     }
+//   });
 
-  formData.append('upload_preset', 'ujpu6gyk');
-  formData.append('file', file);
-  xhr.send(formData);
-}
+//   formData.append('upload_preset', 'ujpu6gyk');
+//   formData.append('file', file);
+//   xhr.send(formData);
+// }
 
 
 function listFile(file) {
